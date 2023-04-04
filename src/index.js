195dc74a4315c46b0914caaa5fc8f0fe4e9af314
lucasoneves <casluhc@gmail.com>
2023-04-04 13:36:43 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        res.statusCode = 200;
+        res.end('Hello, world!');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/ !!!');
+});
