5324662f77ee5a16785c3697e5d07ef51c0e9554
lucasoneves <casluhc@gmail.com>
2023-04-08 22:38:21 -0300

    Testing the api with thunder client

---
+++ src/router.ts
@@ -0,0 +1,39 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/product", (req, res) => {
+  res.json({ message: "success", status: 200 });
+});
+router.get("/product/:id", (req, res) => {
+  res.send({ message: "success", status: 200 });
+})
+router.put("product/:id", (req, res) => { });
+router.post("product", (req, res) => { });
+router.delete("product/:id", (req, res) => { });
+
+/** Update **/
+
+router.get("/update", (req, res) => {
+  res.send("Hello on update page!");
+});
+router.get("/update/:id", (req, res) => {
+  res.send("Hello on home update detail!");
+})
+router.put("update/:id", (req, res) => { });
+router.post("update", (req, res) => { });
+router.delete("update/:id", (req, res) => { });
+
+/** Update Points */
+
+router.get("/updatepoint", (req, res) => {
+  res.send("Hello on updatepoint page!");
+});
+router.get("/updatepoint/:id", (req, res) => {
+  res.send("Hello on home updatepoint detail!");
+})
+router.put("updatepoint/:id", (req, res) => { });
+router.post("updatepoint", (req, res) => { });
+router.delete("updatepoint/:id", (req, res) => { });
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import router from './router';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({message: 'success'})
+  res.json({ message: 'success' })
 });
+
+
+app.use("/api", router);
 
 
 
